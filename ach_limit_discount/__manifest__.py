b1fa4e09bd65ed56476d0d157764d88aea064c55
Myke Rodas <mikealquimia@gmail.com>
2023-04-07 18:57:07 -0600

    first commit v16

---
+++ ach_limit_discount/__manifest__.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Sales Discount Limit",
+    'summary': """
+        Sales discount limit based on sales equipment""",
+    'description': """
+        Sales discount limit on sales order lines, based on limit assigned to sales team
+    """,
+    'author': "Gt Development Alchemy",
+    'license': 'LGPL-3',
+    'support': 'developmentalchemygx@gmail.com',
+    'category': 'Sales',
+    'version': '1.1',
+    'price': 3.00,
+    'currency': 'USD',
+    'depends': ['base', 
+                'sale',
+                'sales_team',
+                'sale_management',
+                'crm'],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'images': ['static/description/banner.png'],
+}
