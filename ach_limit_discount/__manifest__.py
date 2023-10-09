c6b64900a6d0d9fba37bbb5fede0f3210c42f862
Myke Rodas <mrodas@pitaya.tech>
2023-10-09 11:36:46 -0600

    change name

--- ach_limit_discount/__manifest__.py
+++ ach_limit_discount/__manifest__.py
@@ -6,7 +6,7 @@
     'description': """
         Sales discount limit on sales order lines, based on limit assigned to sales team
     """,
-    'author': "ACH, Alchemical Code",
+    'author': "ACH Alchemical Code",
     'license': 'LGPL-3',
     'support': 'developmentalchemygx@gmail.com',
     'category': 'Sales',
